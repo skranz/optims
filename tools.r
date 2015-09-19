31a3924d64749b6ce0f2695fa3d684ee16f2d4df
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2015-09-19 16:05:11 +0200

    First commit

---
+++ tools.r
@@ -0,0 +1,14 @@
+nlist = function (...)
+{
+  li = list(...)
+  li.names = names(li)
+  names = unlist(as.list(match.call())[-1])
+  if (!is.null(li.names)) {
+    no.names = li.names == ""
+    names(li)[no.names] = names[no.names]
+  }
+  else {
+    names(li) = names
+  }
+  li
+}
